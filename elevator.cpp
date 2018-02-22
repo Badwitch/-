0ea2b5c7a59eebca0fb9fcc2a3c499425e881807
Badwitch <36150059+Badwitch@users.noreply.github.com>
2018-02-22 20:42:54 +0800

    Create elevator.cpp
    created an elevator

---
+++ elevator.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<math.h>
+struct request
+{
+	int a;
+	int b;
+	int c;
+}request[5],temp;//结构体数组request用于存入请求,变量temp用于后面排序交换。
+int main()
+{
+ 	int i,t=1,s=0,sum=0;
+ 	for(i=0;i<5;i++)
+ 	{
+ 		scanf("%d %d %d",&request[i].a,&request[i].b,&request[i].c);
+	 }
+	
+	 for(i=1;i<5;i++)
+	 {
+	 	if(request[i].a<request[i-1].a)
+	 	{
+	 		temp=request[i];
+	 		request[i]=request[i-1];
+	 		request[i-1]=temp;
+		 }
+	}//按请求时刻排序 
+		for(i=0;i<5;i++)
+		{
+			s+=request[i].a+fabs(request[i].c-t);
+			t=request[i].c;
+			sum+=s;
+			printf("%d时，停靠在%d楼\n",s,t);
+		 } 
+		 printf("%d",sum);
+	 return 0;
+}
+ 
